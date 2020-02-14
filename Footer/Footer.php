75d19adf5010156dbfd919422fe9fa8b76ce6e54
AliAbuAlRub <51909522+AliAbuAlRub@users.noreply.github.com>
2020-02-14 18:57:27 +0200

    Add files via upload

---
+++ Footer/Footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+</head>
+
+<body>
+    <div class="footer-container">
+        <footer>
+            <!-- <hr style="margin-top: 167px;" /> -->
+            <h2 style="margin-left: 38px;">Contact Us</h2>
+            <ul class="ul-footer">
+                <li>Address: 1234 Al-Irsal, Ramallah, Palestine</li>
+                <li>Phone: +972 597308828, +972 569436161</li>
+                <li>Email: <a href="mailto:aliabuelrub@hotmail.com">palStore@hotmail.com</a></li>
+            </ul>
+        </footer>
+    </div>
+
+</body>
+
+</html>
